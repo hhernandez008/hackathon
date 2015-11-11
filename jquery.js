fa150790b8b63829376ddc426bc4b8b354484d58
hhernandez008 <hhernandez008@gmail.com>
2015-11-11 00:53:34 -0800

    load multiple youtube videos and change when new video is selected

--- jquery.js
+++ jquery.js
@@ -5,6 +5,7 @@ var twitterSearch = apis.twitter;
 
 
 
+
 /**
  * Created by Weeping Beef on 11/10/15.
  */
@@ -20,7 +21,6 @@ function appleRss() {
         url: 'http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topMovies/json',
 
         success: function (response) {
-            console.log(response);
             var movies = response.feed.entry;
             for (var i = 0; i < movies.length; i++) {
                 var image = $('<img>').attr('src', movies[i]['im:image'][2]['label']);
@@ -30,9 +30,14 @@ function appleRss() {
                 $('.itunes').append(imageDiv);
 
             }
+            //Search & append results for first movie
+            movieInfo(movies[0]['im:name']['label']);
+
+            //add click handler to imageDiv
             $('.imgdiv').click(function(){
                 $('.twitter').empty();
                 $('.vine').empty();
+                $(".youtube").empty();
                 movieInfo($(this).find('div').text());
             });
         },
@@ -42,37 +47,35 @@ function appleRss() {
     });
 }
 
+/**
+ * Call the YouTube, Twitter, & Vine apis objects getData methods & pass in the movie name to search.
+ * Append response data to DOM as videos or tweets.
+ * @param movie
+ */
 function movieInfo(movie){
-    youTubeSearch.getData(movie + " Official Trailer","5", function(boolean, response){
+    //YOUTUBE
+    youTubeSearch.getData(movie + " Official Trailer","4", function(boolean, response){
         if(boolean) {
-            //console.log("YouTube", response, "ID:", response.video[1].id);
+            var videos =[];
+            //store video info object in videos array
             for(var i = 0; i < response.video.length; i++) {
-                if($("#player").is("div")){
-                    apis.youtube.playVideo(response.video[1].id, "175", "295");
-                    setTimeout(function () {
-                        apis.youtube.stopVideo()
-                    }, 20000);
-                    $("#player").attr("id", "");
-                }
-                //apis.youtube.playVideo(response.video[1].id, "175", "295");
-                //
-                //setTimeout(function () {
-                //    apis.youtube.stopVideo()
-                //}, 20000);
+                videos[i] = response.video[i]
             }
+            //call placeVideos function to append all of the videos to DOM
+            placeVideos(videos);
+
         } else {
+            //TODO: show error to screen, add a retry button
             console.log("YouTube Failed");
         }
     });
 
-    vineSearch.getData("movie " + movie, function (boolean, data) {
+    //VINE
+    vineSearch.getData(movie, function (boolean, data) {
         if (boolean) {
-            console.log(data);
-
             var vine = data.vines;
             for (i = 0; i < vine.length; i++) {
                 if (vine[i] !== null) {
-                    console.log("vine[" + i + "].html", vine[i].html);
                     var vines = vine[i].html;
                     var vinesParagraph = $("<div>", {
                         html: vines
@@ -81,11 +84,15 @@ function movieInfo(movie){
                 }
             }
 
+        }else{
+            //TODO: show error to screen, add a retry button
+            console.log("Vine Failed");
         }
     });
+
+    //TWITTER
     twitterSearch.getData("movie " + movie, function (boolean, data) {
         if (boolean) {
-            console.log(data);
             var tweet = data.tweets;
             for (i = 0; i < tweet.statuses.length; i++) {
                 var tweets = tweet.statuses[i].text;
@@ -95,17 +102,56 @@ function movieInfo(movie){
                 $(".twitter").append(twitterParagraph);
             }
         } else {
-            console.log(boolean);
-
+            //TODO: show error to screen, add a retry button
+            console.log("Twitter Failed");
         }
     });
 }
 
+/**
+ * Creates divs to show video start image for each item in array param.
+ * Allows for multiple videos to be created at a time without having to load the data of each right away.
+ * Called in the movieInfo function
+ * @param array
+ */
+function placeVideos(array){
+    //create new div for each video in array
+    for(var i = 0; i<array.length; i++){
+        var $videoDiv = $("<div>",{
+                class: "youtubeVideo",
+                id: array[i].id,
+        });
+        $("#youtube").append($videoDiv);
+    }
+
+    //add video player functionality to divs
+    var $videos = $(".youtubeVideo");
+    for(var j = 0; j<$videos.length; j++){
+        var youtubeVideo = $videos[j];
+
+        //add video default image as source
+        var img = $("<img>").attr("src", "http://i.ytimg.com/vi/"+youtubeVideo.id+"/hqdefault.jpg").addClass("thumb");
+        var playButton = $("<div>").addClass("playButton");
+
+        //add videos default image & play button
+        $(youtubeVideo).append(img).append(playButton);
+    }
+}
 
 
 $(document).ready(function () {
     appleRss();
 
+    //Click handler for each YouTube default image to play the video
+    $(".youtube").on("click",".youtubeVideo", function(){
+        //create iframe for video
+        var $iframe = $("<iframe>",{
+            //add video url as source
+            src: "https://www.youtube.com/embed/" + $(this).attr("id") + "?autoplay=1&autohide=1&border=0&wmode=opaque&enablejsapi=1"
+        });
+        //replace the div that holds the default image with the iframe that holds the video
+        $(this).replaceWith($iframe);
+    });
 
 
 
